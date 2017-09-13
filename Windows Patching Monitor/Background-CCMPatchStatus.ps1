b1c1ec1ad82fc41ab60ff8d67ce7a1c2011aaad7
Rohin Sidharth <31369803+RohinSidharth@users.noreply.github.com>
2017-09-13 11:51:24 +0530

    Add files via upload

---
+++ Windows Patching Monitor/Background-CCMPatchStatus.ps1
@@ -0,0 +1,143 @@
+﻿Param
+(
+    # Param1 help description
+    [Parameter(Mandatory=$true,
+                ValueFromPipelineByPropertyName=$true,
+                Position=0)]
+    $FQDN
+)
+$Alive = Test-Connection -ComputerName $FQDN -Count 2 -ErrorAction Stop
+
+$PatchStatusObject = New-Object -TypeName PSObject
+Add-Member -InputObject $PatchStatusObject -MemberType NoteProperty -Name FQDN -Value $FQDN | Out-Null
+Add-Member -InputObject $PatchStatusObject -MemberType NoteProperty -Name Available_Patches -Value $null | Out-Null
+Add-Member -InputObject $PatchStatusObject -MemberType NoteProperty -Name Patching_Progress -Value $null | Out-Null
+Add-Member -InputObject $PatchStatusObject -MemberType NoteProperty -Name Reboot_Required -Value $null | Out-Null
+Add-Member -InputObject $PatchStatusObject -MemberType NoteProperty -Name Last_Boot_Time -Value $null | Out-Null
+Add-Member -InputObject $PatchStatusObject -MemberType NoteProperty -Name Last_WindowsUpdate -Value $null | Out-Null
+Add-Member -InputObject $PatchStatusObject -MemberType NoteProperty -Name Days_SinceReboot -Value $null | Out-Null
+Add-Member -InputObject $PatchStatusObject -MemberType NoteProperty -Name C_FreeSpace_GB -Value $null | Out-Null
+
+
+if ($Alive)
+{
+    try
+    {
+        $ErrorActionPreference = "Stop"
+
+        #Collect Patch Information
+        try{
+            $CCM_All = @(Get-WmiObject -ComputerName $FQDN -Namespace "ROOT\ccm\ClientSDK" -Class CCM_SoftwareUpdate -Property "*")
+            $EvalStatus = $CCM_All.EvaluationState
+            $CCMUpdateStatus = $CCM_All | Where-Object -Property ComplianceState -eq 0
+
+            #Progress
+            if($EvalStatus)
+            {
+                #Initialize
+                $A = $B = $C = $D = $E = $F = 0
+                        
+                foreach ($Code in $EvalStatus)
+                {
+                    Switch -Regex ($Code)
+                    {
+                        {0..3 -contains $_} {$A++}
+                        {4..7 -contains $_} {$C++}
+                        {8..12 -contains $_} {$D++}
+                        {13  -contains $_} {$E++}
+                        {14..23 -contains $Code} {$F++}
+                    }
+                }
+                $Progress = "U=$A, I=$C, C=$D, F=$E, W=$F"
+            }
+            else
+            {
+                $Progress = "----"
+            }
+
+            if($CCMUpdateStatus.Count)
+            {
+                $Count = $CCMUpdateStatus.Count
+                #$PatchStatus = "$FQDN`t$Count`t$Progress`t$Reboot_Required`t$LastBootTime`t$LastWU"
+            }
+            Else
+            {
+                $Count = "ZERO"
+                #$PatchStatus = "$FQDN`t$Count`t$Progress`t$Reboot_Required`t$LastBootTime`t$LastWU"
+            }
+        }
+        catch
+        {
+            $EvalStatus = "SCCM-ClientQueryFailed"
+            $Count = "SCCM-ClientError"
+            $Progress = "Unavailable"
+            $CCMUpdateStatus = "QueryFailed"
+        }
+    
+        try{    
+            $LastBootTime = (Get-WmiObject -ComputerName $FQDN -Class Win32_OperatingSystem | Select-Object __SERVER,@{label='LastBootUpTime';expression={$_.ConvertToDateTime($_.LastBootupTime)}}).LastBootUpTime
+
+            $Today = Get-Date
+            $BootDate = Get-Date($LastBootTime)
+
+            $Diff = $Today - $BootDate
+            $DaysSinceLastReboot ="{0:N1}" -f $Diff.TotalDays
+
+        }
+        catch{
+            $LastBootTime = "BootTimeUnavailable"
+            $DaysSinceLastReboot = "InsufficientData"
+        }
+    
+        try{
+            $LastWU = (Get-HotFix -ComputerName $FQDN | Sort-Object InstalledOn -Descending | Select-Object -First 1).InstalledOn
+        }
+        catch{$LastWU = "LastUpdate_Unavailable"}
+    
+
+        #REBOOT REQUIRED STATUS
+        try{
+            $CCMClientUtil = Get-WmiObject -ComputerName $FQDN -Namespace 'ROOT\ccm\ClientSDK' -Class CCM_ClientUtilities -list
+            $Reboot_Required = $CCMClientUtil.DetermineIfRebootPending().RebootPending
+        }
+        catch{$Reboot_Required = "Unavailable"}
+
+        #DiskSpace
+        Try
+        {
+            $C = (Get-WmiObject win32_logicaldisk -Computername $FQDN -ErrorAction Stop | where-object {$_.DeviceID -eq “C:”})
+            $Size = "{0:N2}" -f ($C.Size/1gb)
+            $FreeSpace = "{0:N2}" -f ($C.FreeSpace/1gb)
+        }
+        catch
+        {
+            $FreeSpace = "----"
+        }
+
+        #$PatchStatus = "$FQDN`t$Count`t$Progress`t$Reboot_Required`t$LastBootTime`t$LastWU`t$FreeSpace"
+        $PatchStatusObject.FQDN = $FQDN
+        $PatchStatusObject.Available_Patches = $Count
+        $PatchStatusObject.C_FreeSpace_GB = $FreeSpace
+        $PatchStatusObject.Days_SinceReboot = $DaysSinceLastReboot
+        $PatchStatusObject.Last_Boot_Time = $LastBootTime
+        $PatchStatusObject.Last_WindowsUpdate = $LastWU
+        $PatchStatusObject.Patching_Progress = $Progress
+        $PatchStatusObject.Reboot_Required = $Reboot_Required
+
+    }
+    catch
+    {
+        $Errormsg = $_.ErrorDetails.Message
+        #$PatchStatus = "$FQDN`tACCESS_DENIED`t$Errormsg"
+        $PatchStatusObject.FQDN = $FQDN
+        $PatchStatusObject.Available_Patches = $Errormsg
+    }
+}
+else
+{
+    #$PatchStatus = "$FQDN`tUNREACHABLE"
+    $PatchStatusObject.FQDN = $FQDN
+    $PatchStatusObject.Available_Patches = "UNREACHABLE"
+}
+$ErrorActionPreference = "Continue"
+Return $PatchStatusObject
---
+++ Windows Patching Monitor/PatchResultTable.ps1
@@ -0,0 +1,70 @@
+﻿$FQDNList = Get-content .\FQDNList.txt
+
+$C = 1
+$PatchingTable = foreach ($FQDN in $FQDNList)
+{
+    $ServerObject = New-Object PSObject
+    Add-Member -InputObject $ServerObject -MemberType NoteProperty -Name SL -Value $C | Out-Null
+    Add-Member -InputObject $ServerObject -MemberType NoteProperty -Name FQDN -Value $FQDN | Out-Null
+    Add-Member -InputObject $ServerObject -MemberType NoteProperty -Name Available_Patches -Value $null | Out-Null
+    Add-Member -InputObject $ServerObject -MemberType NoteProperty -Name Patching_Progress -Value $null | Out-Null
+    Add-Member -InputObject $ServerObject -MemberType NoteProperty -Name Reboot_Required -Value $null | Out-Null
+    Add-Member -InputObject $ServerObject -MemberType NoteProperty -Name Last_Boot_Time -Value $null | Out-Null
+    Add-Member -InputObject $ServerObject -MemberType NoteProperty -Name Last_WindowsUpdate -Value $null | Out-Null
+    Add-Member -InputObject $ServerObject -MemberType NoteProperty -Name Days_SinceReboot -Value $null | Out-Null
+    Add-Member -InputObject $ServerObject -MemberType NoteProperty -Name C_FreeSpace_GB -Value $null | Out-Null
+    Add-Member -InputObject $ServerObject -MemberType NoteProperty -Name RoW_Updated -Value $null | Out-Null
+    Add-Member -InputObject $ServerObject -MemberType NoteProperty -Name Record_LastUpdated -Value (Get-Date) | Out-Null
+    $C++ | Out-Null
+    $ServerObject
+}
+#$PatchingTable | Select-Object -Property FQDN, Available_Patches, Patching_Progress, Reboot_Required, Last_Boot_Time, Last_WindowsUpdate, Days_SinceReboot, C_FreeSpace_GB, RoW_Updated | ft
+
+Do{
+    foreach ($Row in $PatchingTable)
+    {
+        try
+        {
+            $JobStatus = (Get-Job -Name $Row.FQDN -ErrorAction Stop).State
+        }
+        catch
+        {
+            $JobStatus = "Non-Existent"
+        }
+
+        if ($JobStatus -eq "Completed")
+        {
+            $PatchStatusObject = Receive-Job -Name $Row.FQDN
+
+            $Row.Available_Patches = $PatchStatusObject.Available_Patches
+            $Row.C_FreeSpace_GB = $PatchStatusObject.C_FreeSpace_GB
+            $Row.Days_SinceReboot = $PatchStatusObject.Days_SinceReboot
+            $Row.Last_Boot_Time = $PatchStatusObject.Last_Boot_Time
+            $Row.Last_WindowsUpdate = $PatchStatusObject.Last_WindowsUpdate
+            $Row.Patching_Progress = $PatchStatusObject.Patching_Progress
+            $Row.Reboot_Required = $PatchStatusObject.Reboot_Required
+            $Row.Record_LastUpdated = (Get-Date)
+            $Row.RoW_Updated = "1 Sec Ago"
+
+            Remove-Job -Name $Row.FQDN -Force | Out-Null            
+            Start-Job -Name $Row.FQDN -FilePath .\Background-CCMPatchStatus.ps1 -ArgumentList $Row.FQDN | Out-Null
+        }
+        elseif($JobStatus -eq "Non-Existent")
+        {
+            $StartTime = Get-Date
+            Start-Job -Name $Row.FQDN -FilePath .\Background-CCMPatchStatus.ps1 -ArgumentList $Row.FQDN | Out-Null
+            $Row.Record_LastUpdated = (Get-Date)
+        }
+        else
+        {
+            $CurrentTime = Get-Date
+            $PreviousUpdateTime = $Row.Record_LastUpdated
+            [int]$Sec = ($CurrentTime - $PreviousUpdateTime).TotalSeconds
+            $Row.Row_Updated = "$Sec Sec Ago"
+        }
+    }
+    Clear-Host
+    $PatchingTable | Sort-Object -Property SL | Select-Object -ExcludeProperty Record_LastUpdated | Format-Table -AutoSize
+    Start-Sleep -Milliseconds 500
+}
+While(1 -gt 0)
